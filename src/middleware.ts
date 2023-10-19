45dbb2a6b40d3799dd2b45f9fc7b426b8da4dfcb
jang_yoonsu <shagrat8546@gmail.com>
2023-10-20 01:30:52 +0900

    fix : middleware redirect 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,18 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (
-    request.cookies.get("session-id") &&
-    request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  const sessionId = request.cookies.get("session-id");
+  const isLoggedIn = !!sessionId;
+
+  if (isLoggedIn && request.nextUrl.pathname.startsWith("/auth")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (
-    !request.cookies.get("session-id") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  if (!isLoggedIn && !request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  if (!isLoggedIn && request.nextUrl.pathname.startsWith("/my-page")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 }
