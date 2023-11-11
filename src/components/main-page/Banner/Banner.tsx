e21b8a1ca9f998e2ae73756bca7ab76f996aeb7b
jang_yoonsu <shagrat8546@gmail.com>
2023-11-12 02:26:57 +0900

    fix : mbti, 이상형월드컵 배너 임시 제거

--- src/components/main-page/Banner/Banner.tsx
+++ src/components/main-page/Banner/Banner.tsx
@@ -1,96 +1,83 @@
 "use client";
 
+// import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styled, { css } from "styled-components";
 import BannerSearchBar from "./BannerSearchBar";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
+// import Link from "next/link";
+// import { useWindow } from "@/src/hooks/useWindow";
 
 const Banner = () => {
-  const [currentBannerIndex, setCurrentBannerIndex] = useState<number>(0); // 현재 배너 인덱스
-  const [translateValue, setTranslateValue] = useState<number>(0); // 현재 위치
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // 윈도우창 너비
-  const sliderRef = React.useRef<HTMLDivElement>(null); // 배너 슬라이드 div
-  const bannerWrapperWidth = sliderRef.current
-    ? sliderRef.current.offsetWidth
-    : 0;
+  // const [currentBannerIndex, setCurrentBannerIndex] = useState<number>(0); // 현재 배너 인덱스
+  // const [translateValue, setTranslateValue] = useState<number>(0); // 현재 위치
+  // const { windowWidth } = useWindow();
+  // const sliderRef = React.useRef<HTMLDivElement>(null); // 배너 슬라이드 div
+  // const bannerWrapperWidth = sliderRef.current
+  //   ? sliderRef.current.offsetWidth
+  //   : 0;
 
-  const slideChildLength = windowWidth >= 1024 ? 3 : 2;
+  // const slideChildLength = windowWidth >= 1024 ? 3 : 2;
 
-  useEffect(() => {
-    // 브라우저 크기가 변경될 때마다 이벤트 핸들러를 호출하여 windowWidth를 업데이트
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+  // /** 배너 인덱스 계산해서 다음 배너로 슬라이드해주는 함수 */
+  // const slide = (currentBannerIndex: number) => {
+  //   if (currentBannerIndex > slideChildLength - 2) {
+  //     setCurrentBannerIndex(0);
+  //   } else {
+  //     setCurrentBannerIndex(currentBannerIndex + 1);
+  //   }
+  // };
 
-    // 컴포넌트가 마운트될 때와 화면 크기가 변경될 때마다 이벤트 리스너를 추가하고 제거합
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  // useEffect(() => {
+  //   if (currentBannerIndex === 0) {
+  //     setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
+  //   }
+  //   if (currentBannerIndex === 1) {
+  //     setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
+  //   }
+  //   if (currentBannerIndex === 2) {
+  //     setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
+  //   }
+  // }, [currentBannerIndex, bannerWrapperWidth]);
 
-  /** 배너 인덱스 계산해서 다음 배너로 슬라이드해주는 함수 */
-  const slide = (currentBannerIndex: number) => {
-    if (currentBannerIndex > slideChildLength - 2) {
-      setCurrentBannerIndex(0);
-    } else {
-      setCurrentBannerIndex(currentBannerIndex + 1);
-    }
-  };
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     slide(currentBannerIndex);
+  //   }, 8000);
 
-  useEffect(() => {
-    if (currentBannerIndex === 0) {
-      setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
-    }
-    if (currentBannerIndex === 1) {
-      setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
-    }
-    if (currentBannerIndex === 2) {
-      setTranslateValue(-bannerWrapperWidth * currentBannerIndex);
-    }
-  }, [currentBannerIndex, bannerWrapperWidth]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      slide(currentBannerIndex);
-    }, 8000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [currentBannerIndex]);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [currentBannerIndex]);
 
   return (
     <BannerContainer>
       <BannerWindow>
         <SliderContainer>
-          <Slider ref={sliderRef} translateValue={translateValue}>
-            <BannerWrapper2>
-              <SearchArea>
-                <SearchTextContainer>
-                  <SearchTextWrapper>
-                    대한민국 No.1 레시피 커뮤니티 맛이슈에서
-                  </SearchTextWrapper>
-                  <SearchTextWrapper>
-                    찾으시는 레시피를 검색해보세요!
-                  </SearchTextWrapper>
-                </SearchTextContainer>
-                <BannerSearchBar />
-              </SearchArea>
-              <ImageArea>
-                <ImageWrapper>
-                  <Image
-                    src="/images/banner/bannerImage1.png"
-                    alt="banner"
-                    width={450}
-                    height={280}
-                    priority
-                  />
-                </ImageWrapper>
-              </ImageArea>
-            </BannerWrapper2>
-            <BannerWrapper>
+          {/* <Slider ref={sliderRef} translateValue={translateValue}> */}
+          <BannerWrapper2>
+            <SearchArea>
+              <SearchTextContainer>
+                <SearchTextWrapper>
+                  대한민국 No.1 레시피 커뮤니티 맛이슈에서
+                </SearchTextWrapper>
+                <SearchTextWrapper>
+                  찾으시는 레시피를 검색해보세요!
+                </SearchTextWrapper>
+              </SearchTextContainer>
+              <BannerSearchBar />
+            </SearchArea>
+            <ImageArea>
+              <ImageWrapper>
+                <Image
+                  src="/images/banner/bannerImage1.png"
+                  alt="banner"
+                  width={450}
+                  height={280}
+                />
+              </ImageWrapper>
+            </ImageArea>
+          </BannerWrapper2>
+          {/* <BannerWrapper>
               <Link href="/mbti">
                 <Image
                   src="/images/banner/banner22.jpg"
@@ -111,10 +98,10 @@ const Banner = () => {
                   alt="banner3"
                 />
               </Link>
-            </BannerWrapper>
-          </Slider>
+            </BannerWrapper> */}
+          {/* </Slider> */}
         </SliderContainer>
-        <IndicatorWrapper>
+        {/* <IndicatorWrapper>
           <IndicatorDot
             active={0 === currentBannerIndex}
             onClick={() => setCurrentBannerIndex(0)}
@@ -127,7 +114,7 @@ const Banner = () => {
             active={2 === currentBannerIndex}
             onClick={() => setCurrentBannerIndex(2)}
           />
-        </IndicatorWrapper>
+        </IndicatorWrapper> */}
       </BannerWindow>
     </BannerContainer>
   );
