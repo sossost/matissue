3947bac0daa59d927691d85b96ddb1c558041eeb
jang_yoonsu <shagrat8546@gmail.com>
2023-11-21 15:05:52 +0900

    fix : 쿼리 클라이언트 기본 stale, cache타임 제거

--- src/context/ReactQueryContext.tsx
+++ src/context/ReactQueryContext.tsx
@@ -11,8 +11,6 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 60,
     },
   },
 });
